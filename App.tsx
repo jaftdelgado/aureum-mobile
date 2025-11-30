0ae329f3185d7d289d70551f388e2e4b0dc3d7e3
Cesar <cecm1010@gmail.com>
2025-11-29 20:49:09 -0600

    add: Log out button

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-// src/app/App.tsx
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
