4af4ea8c3f896ef55e558ddc4553d89b9f61e4ae
Cesar <cecm1010@gmail.com>
2025-12-25 13:35:15 -0600

    build(config): setup android apk build profile and permissions

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,6 +1,6 @@
 export const ENV = {
   ASSETS_API_URL: process.env.EXPO_PUBLIC_API_GATEWAY_URL || '', 
-  API_GATEWAY_URL: process.env.EXPO_PUBLIC_API_GATEWAY_URL || 'http://localhost:3000',
+  API_GATEWAY_URL: process.env.EXPO_PUBLIC_API_GATEWAY_URL,
   SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL || '',
   SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '',
 };
